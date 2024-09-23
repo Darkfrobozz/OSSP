c50fd2837d1083cab787ec5e038221eb5d0feeea
darkfrobozz <darkfrobozz.mega@gmail.com>
2024-09-23 15:19:37 +0200

    pipieline refactored for error handling and switch case error

--- mandatory/src/pipeline.c
+++ mandatory/src/pipeline.c
@@ -8,40 +8,53 @@
 
 void child_a(int fd[]) {
   dup2(fd[WRITE], STDOUT_FILENO);
-  // We can close the pipe reader
+  // We can close the unused descriptors
   close(fd[READ]);
+  close(fd[WRITE]);
 
   // This should be done last leaves pipeline.c
   execlp("ls", "ls", "-F", "-1", NULL);
+  perror("Could not switch executable, child a");
+  exit(EXIT_FAILURE);
 }
 
 void child_b(int fd[]) {
   dup2(fd[READ], STDIN_FILENO);
-  // We can close the pipe reader
+  // We can close the unused
   close(fd[WRITE]);
+  close(fd[READ]);
 
   // Same as in child a
   execlp("nl", "nl", NULL);
+  perror("Could not switch executable, child b");
+  exit(EXIT_FAILURE);
 }
 
 int main(void) {
   int fd[2];
   if (pipe(fd) == -1) {
     perror("Could not create pipe");
+    exit(EXIT_FAILURE);
   }
 
   switch (fork()) {
   case 0:
     child_a(fd);
+    break;
   case -1:
     perror("Could not fork");
+    exit(EXIT_FAILURE);
+    break;
   }
 
   switch (fork()) {
   case 0:
     child_b(fd);
+    break;
   case -1:
     perror("Done");
+    exit(EXIT_FAILURE);
+    break;
   }
 
   // Parent does not need pipes, so close them
@@ -49,8 +62,8 @@ int main(void) {
   close(fd[1]);
 
   // Reuse fd for status
-  wait(fd);
+  wait(NULL);
   // printf("%d\n", *fd);
-  wait(fd + 1);
+  wait(NULL);
   // printf("%d\n", *(fd + 1));
 }
